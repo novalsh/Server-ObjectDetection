5c759b3807ad1b5ec131f2f0ebb937ba9788134c
novalsh <muhammad.harjana@binus.ac.id>
2023-04-18 19:59:03 +0700

    createHistory

--- app/controllers/HistoryController.js
+++ app/controllers/HistoryController.js
@@ -19,5 +19,25 @@ const getHistory = async (req, res) => {
     }
 }
 
-module.exports = { getHistorys, getHistory };
+const createHistory = async (req, res) => {
+    try {
+      const { name, description, date, photo_url, user_id, branch_id } = req.body;
+      const dataHistory = await History.create({
+        name,
+        description,
+        date,
+        photo_url,
+        user_id,
+        branch_id,
+        status: 'aman' // status default adalah 'aman'
+      });
+      res.json(dataHistory);
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ message: 'Terjadi kesalahan saat menyimpan data.' });
+    }
+  };
+
+
+module.exports = { getHistorys, getHistory, createHistory };
 
--- app/routes/History.js
+++ app/routes/History.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const { getHistorys, getHistory} = require('../controllers/HistoryController');
+const { getHistorys, getHistory, createHistory} = require('../controllers/HistoryController');
 const { protect } = require('../controllers/AuthController');
 
 const router = express.Router();
 
 router.get('/api/history', protect, getHistorys);
 router.get('/api/history/:id', protect, getHistory);
+router.post('/api/history', protect, createHistory);
 
 module.exports = router;
