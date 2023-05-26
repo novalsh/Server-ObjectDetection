0f81608c3b6938c1e74e72d55f94290297b3dd7e
Hadad Al Akbar <75150256+qweersq@users.noreply.github.com>
2023-05-27 06:37:59 +0700

    remove protect create branch route

--- app/routes/Branch.js
+++ app/routes/Branch.js
@@ -8,7 +8,7 @@ const router = express.Router();
 router.get('/api/branch',protect, getBranches);
 router.get('/api/branch/:id',protect, getBranch);
 router.get('/api/branches/token',protect, getBranchByToken);
-router.post('/api/branch', protect, createBranch);
+router.post('/api/branch', createBranch);
 router.put('/api/branch/:id', protect, updateBranch);
 router.delete('/api/branch/:id', protect, deleteBranch);
 
