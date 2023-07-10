8e26d9430b12dfef563c0013a749b69ea0889a35
Hadad Al Akbar <75150256+qweersq@users.noreply.github.com>
2023-07-10 23:00:01 +0700

    update seeder

--- Seeders/1.branchSeeders.js
+++ Seeders/1.branchSeeders.js
@@ -19,6 +19,7 @@ module.exports = {
         createdAt: new Date(),
         updatedAt: new Date()
       },
+      
     ]);
   },
 
--- Seeders/2.usersSeeders.js
+++ Seeders/2.usersSeeders.js
@@ -25,6 +25,7 @@ module.exports = {
         createdAt: new Date(),
         updatedAt: new Date()
       },
+      
     ]);
   },
 
