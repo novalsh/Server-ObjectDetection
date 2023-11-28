fa8216543ba9ceebd08ebc11f0337b4c49cf767d
Hadad Al Akbar <75150256+qweersq@users.noreply.github.com>
2023-11-28 15:02:03 +0700

    update

--- config/database.js
+++ config/database.js
@@ -2,16 +2,16 @@ const { Sequelize } = require('sequelize');
 require('dotenv').config(); 
 const env = process.env.NODE_ENV || 'development'; 
 
-const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USERNAME, {
+const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
   dialect: process.env.DB_DIALECT,
   port: process.env.DB_PORT,
   // dialectOptions: {
-    // ssl: {
-    //   require: true,
-    //   rejectUnauthorized: false
-    // },
-    // connectTimeout: 20000,
+  //   ssl: {
+  //     require: true,
+  //     rejectUnauthorized: false
+  //   },
+  //   connectTimeout: 20000,
   // },
 });
 
