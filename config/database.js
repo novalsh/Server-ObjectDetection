fecfc2ae99161942414a02288e1849dfd419eac7
Hadad Al Akbar <75150256+qweersq@users.noreply.github.com>
2023-11-28 14:54:16 +0700

    update

--- config/database.js
+++ config/database.js
@@ -6,13 +6,13 @@ const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USERNAME
   host: process.env.DB_HOST,
   dialect: process.env.DB_DIALECT,
   port: process.env.DB_PORT,
-  dialectOptions: {
+  // dialectOptions: {
     // ssl: {
     //   require: true,
     //   rejectUnauthorized: false
     // },
-    connectTimeout: 20000,
-  },
+    // connectTimeout: 20000,
+  // },
 });
 
 // const sequelize = new Sequelize(process.env.DATABASE_URL, {
